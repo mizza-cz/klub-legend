75bd49af890de34beb06cc6c3e880e6a2fa1d1ca
mizza-cz <mizza.cz666@gmail.com>
2024-05-07 18:27:16 +0200

    add member

---
+++ src/components/member-section/member-section.js
@@ -0,0 +1,8 @@
+$(".popup-youtube, .popup-vimeo, .popup-gmaps").magnificPopup({
+  disableOn: 700,
+  type: "iframe",
+  mainClass: "mfp-fade",
+  removalDelay: 160,
+  preloader: false,
+  fixedContentPos: false,
+});
